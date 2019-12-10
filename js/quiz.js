2d7f38307d727a9688775874f77115e0fcc2d2b7
komine masatake <m-komine@fubic.com>
2019-12-11 07:02:28 +0900

    ジャンルの選択を追加

--- js/quiz.js
+++ js/quiz.js
@@ -1,3 +1,7 @@
+let q_sel = 3; //選択肢の数
+let count; //問題番号
+let ansers = new Array(); //解答記録
+
 // 配列をランダムにする
 Array.prototype.shuffle = function() {
     var i = this.length;
@@ -10,17 +14,65 @@ Array.prototype.shuffle = function() {
     return this;
 }
 
-//初期設定
-function setReady() {
-    count = 0; //問題番号
-	ansers = new Array(); //解答記録
+// 初期設定
+function initialSetting(){
+	var select = document.getElementById('genre');
+	select.onchange = function()
+	{
+		// 選択されているoption要素を取得する
+		var selectedItem = this.options[ this.selectedIndex ];
+		console.log( "選択：" + selectedItem.value );
+		if (selectedItem.value != ""){
+
+			// 表示の制御
+			document.getElementById('select_genre').style.display = "none";
+			document.getElementById('quiz').style.visibility = "visible";
+			document.getElementById('txt_genre').insertAdjacentHTML('afterbegin',selectedItem.text);
+			loadDat( selectedItem.value , function() {
+				console.log('script loaded');
+				// クイズの準備
+				quizReady();
+			});
+
+		}
+	}
+}
+
+// 外部データの読み込み
+function　loadDat(num ,callback){
+	var done = false;
+	var script = document.createElement("script")
+	script.type = "text/javascript";
+	script.src = "./js/quiz_" + num + ".dat";
+	document.body.appendChild(script);
+	console.log( script.src );
+
+    // 読み込みが終わったら処理を行う
+    script.onload = script.onreadystatechange = function() {
+		console.log( this.readyState );
+        if ( !done && (!this.readyState || this.readyState === "loaded" || this.readyState === "complete") ) {
+            done = true;
+            callback();
+            // 読み込んだ後に形跡を削除する
+            script.onload = script.onreadystatechange = null;
+            if ( script.parentNode ) {
+				document.body.removeChild( script );
+				console.log( "remove script" );
+            }
+        }
+    };
+  };
+
+// クイズの開始処理
+function quizReady() {
+	count = 0; //問題番号初期化
     qa.shuffle();
 	//最初の問題
-	quiz();
+	quizDisplay();
 }
 
 //問題表示
-function quiz() {
+function quizDisplay() {
 	var s, n;
 	//問題
 	document.getElementById("text_q").innerHTML = (count + 1) + "問目：" + qa[count][0];
@@ -60,7 +112,7 @@ function anser(num) {
 	count++;
 	//if (count < qa.length) {
     if (count < max_count) {
-		quiz();
+		quizDisplay();
 	} else {
 		//終了
 		s = "<table border='2'><caption>成績発表</caption>";
@@ -84,7 +136,8 @@ function anser(num) {
         //次の選択肢
         s = "";
 		//s += "【<a href='javascript:history.back()'>前のページに戻る</a>】";
-		s += "【<a href='javascript:setReady()'>同じ問題を最初から</a>】";
+		s += "【<a href='javascript:location.reload()'>ジャンルを選び直す</a>】";
+		s += "【<a href='javascript:quizReady()'>同じ問題を最初から</a>】";
 		//s += "【<a href=''>次の問題に進む</a>】";
 		document.getElementById("text_s").innerHTML = s;
 	}
